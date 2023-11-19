cfc56c19044af7f0f72d0b67ff92756c1e9626b6
dean-jet-ink <rr7i5fvv@gmail.com>
2023-11-19 11:52:21 +0900

    tailwind.css.configの修正

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -558,7 +558,7 @@ const config: Config = {
         "100vh": "100vh",
       },
       boxShadow: {
-        blur: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;",
+        blur: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
